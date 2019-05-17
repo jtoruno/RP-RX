cf7f6b589455a59ce2240e9f59a159ba408c3809
dev1 <jtoru@zimplifica.com>
2019-05-16 19:49:46 -0600

    16/05/2019 v2

---
+++ Application/src/test/java/com/zimplifica/redipuntos/ShadowApp.kt
@@ -0,0 +1,12 @@
+package com.zimplifica.redipuntos
+
+import android.app.Application
+import org.robolectric.annotation.Implementation
+import org.robolectric.annotation.Implements
+import org.robolectric.shadows.ShadowApplication
+
+/*
+@Implements(Application::class)
+ class  ShadowApp {
+
+}*/
